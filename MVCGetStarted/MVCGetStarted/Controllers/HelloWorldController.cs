15e2ac7cb2a3b1dfa31e31c81ba38df9c7ebeb0e
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-12 21:49:18 +1100

    refreshing asp.net core mvc

---
+++ MVCGetStarted/MVCGetStarted/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCGetStarted.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld
+        public String Index()
+        {
+            return "Default Action this is";
+        }
+
+        // GET: /HelloWorld/Welcome
+        public String Welcome()
+        {
+            return "Welcome action method!";
+        }
+    }
+}
