161ddf1f08ed0f6a268f2b7895af6c0b2998d94f
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-12 22:43:34 +1100

    Got the C and the V just need the M now

--- MVCGetStarted/MVCGetStarted/Controllers/HelloWorldController.cs
+++ MVCGetStarted/MVCGetStarted/Controllers/HelloWorldController.cs
@@ -9,16 +9,48 @@ namespace MVCGetStarted.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld
-        public String Index()
+        
+        public IActionResult Index()
         {
-            return "Default Action this is";
+            return View();
         }
 
+        public IActionResult Welcome(string name, int numTimes = 5)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        // GET: /HelloWorld
+        /*public String Index()
+        {
+            return "Default Action this is";
+        }*/
+
         // GET: /HelloWorld/Welcome
+        /*
         public String Welcome()
         {
             return "Welcome action method!";
-        }
+        }*/
+
+        // GET: /HelloWorld/Welcome?name=Sam
+        /*
+        public String Welcome(string name)
+        {
+            return HtmlEncoder.Default.Encode($"Welcome {name}");
+        }*/
+
+        // GET: /HelloWorld/Welcome?name=Sam&ID=3882 or /HelloWorld/Welcome/3882?name=Sam
+        // ID matches route parameter
+        // The Welcome method contains a parameter id that matched the URL template in the MapControllerRoute method.
+        // The trailing ? starts the query string
+
+        /*public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
     }
 }
